1c05c06f4f5626b3ae39b20e08ba91bcc096acc6
sht24 <agosantihernandez@outlook.com>
2022-03-30 15:27:24 -0500

    Full generations

--- Reto #2/main.py
+++ Reto #2/main.py
@@ -63,7 +63,7 @@ for i in range(cant):
     evaluacion_hijos = evaluacion(mutacion_hijos, cromosomas, genes, Pesos, Calorias)
 
     # Paso 7, Generando población nueva
-    nueva_gen = nueva_generacion(mutacion_hijos, evaluacion_hijos)
+    poblacion = nueva_generacion(mutacion_hijos, evaluacion_hijos)
 
 
 
