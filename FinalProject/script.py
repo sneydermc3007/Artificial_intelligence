989c959405c0cb25850e81fb447b697fc92835fc
Sneyder <sneider.caicedo@gmail.com>
2022-05-24 18:04:17 -0500

    Select column

--- FinalProject/script.py
+++ FinalProject/script.py
@@ -1,26 +1,26 @@
-from time import time
 import streamlit as st
 import pandas as pd
-from backEnd import *
-
+# from backEnd import *
 
 st.title('K-Means Clustering')
 uploaded_file = st.file_uploader("Choose a file", type=["csv"])
-if st.button("Cargar"):
-    if uploaded_file is not None:
-        file = uploaded_file
-        df = pd.read_csv(file)
-        df.head()
-        st.write(df)
 
-        print("=======================================================")
-        df = pd.DataFrame(df)
+if uploaded_file is not None:
+    file = uploaded_file
+    df = pd.read_csv(file)
+    df.head()
+    st.write(df)
 
-        st.subheader("Columnas del Dataset")
-        df.columns.values
-        print(df.columns.values)
+    print("=======================================================")
+    df = pd.DataFrame(df)
+    st.subheader("Columnas del Dataset")
+    df.columns.values
+    print(df.columns.values)
+    
+    columns_name = df.columns.values
+    option = st.selectbox( 'Seleccione la primera columna: ', options=columns_name, index=0)
+    st.write('Has selecionado en tu primera columna:', option)
 
-        columns_name = df.columns.values
+    option = st.selectbox( 'Seleccione la segunda columna: ', options=columns_name, index=0)
+    st.write('Has selecionado en tu sugunda columna:', option)
 
-        option = st.selectbox( 'Seleccione la primera columna: ', options=columns_name, index=0)
-        st.write('Has selecionado:', option)
